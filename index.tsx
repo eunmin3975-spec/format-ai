902deb77cf3f680533cc42427891f6cc25e7fd4a
eunmin3975-spec <eunmin3975@gmail.com>
2026-01-28 14:52:55 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,18 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// 브라우저에서 직접 실행할 때는 상단 import 문들을 제거하거나 아래처럼 수정해야 합니다.
+const React = window.React;
+const ReactDOM = window.ReactDOM;
+// App 컴포넌트와 CSS는 이미 HTML에서 경로를 잡았으므로 관리를 위해 아래처럼 구성합니다.
+
 import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  console.error("root 엘리먼트를 찾을 수 없습니다.");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    React.createElement(React.StrictMode, null, 
+      React.createElement(App)
+    )
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
